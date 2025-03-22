94e4cd3e8aa9fe918a37d4bfa7768839c5007b5d
amirali <ososgol@gmail.com>
2025-03-22 22:33:31 +0330

    add handle command for checking absent_users

---
+++ Attendance_app/management/commands/calculate_absences.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django.utils.timezone import now
+
+from django.core.management.base import BaseCommand
+
+from Attendance_app.models import AttendanceUser, AbsenceRecord
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'Calculate and record absences for the day'
+
+    def handle(self, *args, **options):
+        today = now().date()
+        present_users = AttendanceUser.objects.filter(created_date=today).values_list('user', flat=True)
+        absent_users = User.objects.exclude(id__in=present_users.values_list('id', flat=True))
+        if absent_users:
+            absent_record, created = AbsenceRecord.objects.get_or_create(date=today)
+            absent_record.absent_users.add(*absent_users)
+            print(absent_record.absent_users.all())
+            absent_record.save()
+
