4337e9938ff423b468a68c327f91b9d407d677ee
amirali <ososgol@gmail.com>
2025-04-17 20:56:00 +0330

    fix delays and add notification if a user had a delay

--- Attendance_app/views.py
+++ Attendance_app/views.py
@@ -261,6 +261,9 @@ def start_attendance_view(request):
             delay_object = Delay.objects.create(user=request.user, delay_time=delay_time)
             attendance_obj.delay = delay_object
             print(f'delay_object={delay_object}')
+            message = f'${request.user} به مدت {delay_time} تاخیر داشته است!'
+            AbsenceWarning.objects.create(user_id=request.user.id , message=message)
+            AbsenceWarning.objects.create(user_id=request.user.created_who.id, message=message)
         if attendance_obj.job_time >= current_shift.required_time:
             attendance_obj.overtime_check = True
     elif overtime:
