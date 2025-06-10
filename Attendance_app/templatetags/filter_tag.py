0d3b3d4dad7a4a869d907e1e0d9cb838613153dd
amirali <ososgol@gmail.com>
2025-06-10 20:34:55 +0330

    add formatted_price to filter tag.py

--- Attendance_app/templatetags/filter_tag.py
+++ Attendance_app/templatetags/filter_tag.py
@@ -1,19 +1,27 @@
 from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.utils import translation
 
 register = template.Library()
 
 def cutter(value, arg):
     return value[:arg]
+
 register.filter("cutter", cutter)
 
 
-
 @register.filter
-def zip(list1, list2):
+def zip_lists(list1, list2):
     return zip(list1, list2)
 
-register = template.Library()
 
 @register.filter
 def is_selected(day, shift):
     return day in shift.work_days.all()
+
+
+@register.filter
+def formatted_price(price):
+    with translation.override('en'):
+        return intcomma(int(price)) if price else None
+
